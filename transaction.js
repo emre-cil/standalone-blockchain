48f84a7b27ee032d3cbb5220f32a80d85064b86a
Melike <meliketekin2243@gmail.com>
2023-06-24 21:03:43 +0300

    update: transaction

--- transaction.js
+++ transaction.js
@@ -1,76 +1,35 @@
-const Block = require('./block');
-const Transaction = require('./transaction');
+const cryptoHash = require('./cryptoHash');
 
-class Blockchain {
-  constructor() {
-    this.chain = [this.createGenesisBlock()];
-    this.difficulty = 4; // Difficulty for mining blocks
-    this.pendingTransactions = [];
-    this.miningReward = 100; // Reward for mining a block
+class Transaction {
+  constructor(sender, recipient, amount) {
+    this.sender = sender;
+    this.recipient = recipient;
+    this.amount = amount;
+    this.timestamp = Date.now();
+    this.signature = '';
   }
 
-  createGenesisBlock() {
-    return new Block(0, Date.now(), 'Genesis Block', '0');
+  calculateHash() {
+    return cryptoHash(this.sender + this.recipient + this.amount + this.timestamp);
   }
 
-  getLatestBlock() {
-    return this.chain[this.chain.length - 1];
+  signTransaction(wallet) {
+    const data = this.calculateHash();
+    this.signature = wallet.sign(data);
   }
 
-  minePendingTransactions(miningRewardAddress) {
-    const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
-    this.pendingTransactions.push(rewardTx);
-
-    const block = new Block(
-      this.chain.length,
-      Date.now(),
-      this.pendingTransactions,
-      this.getLatestBlock().hash
-    );
-    block.mineBlock(this.difficulty);
-
-    this.chain.push(block);
-
-    this.pendingTransactions = [];
-  }
-
-  addTransaction(transaction) {
-    this.pendingTransactions.push(transaction);
-  }
-
-  getBalanceOfAddress(address) {
-    let balance = 0;
-
-    for (const block of this.chain) {
-      for (const transaction of block.data) {
-        if (transaction.sender === address) {
-          balance -= transaction.amount;
-        }
-        if (transaction.recipient === address) {
-          balance += transaction.amount;
-        }
-      }
+  isValid() {
+    if (this.sender === null) {
+      return true; // Genesis transaction
     }
 
-    return balance;
-  }
-
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
-      if (currentBlock.hash !== currentBlock.calculateHash()) {
-        return false;
-      }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        return false;
-      }
+    if (!this.signature || this.signature.length === 0) {
+      throw new Error('No signature found for this transaction');
     }
 
-    return true;
+    const publicKey = ec.keyFromPublic(this.sender, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
   }
 }
 
-module.exports = Blockchain;
+module.exports = Transaction;
